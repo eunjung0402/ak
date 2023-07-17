7db1b32867e63c6d42493e67c3673d802f787e7c
Eun Jung <76139130+eunjung0402@users.noreply.github.com>
2023-07-17 20:43:11 +0900

    Add files via upload
    변수 수정

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,13 @@
 $(function(){
     // main visual swiper
-    var mainSwiper = new Swiper(".visual_swiper", {
-        centeredSlides: true,
+    const mainSwiper = new Swiper(".visual_swiper", {
         autoplay: {
           delay: 5000,
-          disableOnInteraction: false,
+          disableOnInteraction: false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
         },
         speed: 1000,
         loop: true,
-        loopAdditionalSlides: 1,
+        loopAdditionalSlides: 1,  // 슬라이드 반복 시 마지막 슬라이드에서 다음 슬라이드가 보여지지 않는 현상 수정
         pagination: {
           el: ".swiper-pagination",
           type: "fraction",
@@ -19,42 +18,42 @@ $(function(){
         },
     });
 
-      // 메인슬라이드 일시정지
-      $('.pause_btn').click(function(){
-        $('.restart_btn').show();
-        $('.pause_btn').hide();
+      // 메인슬라이드 일시정지 재실행 동작
+    $('.sc_visual .pause_box button').click(function(){
+      $(this).hide().siblings().show();
+      $(this).attr("aria-disabled", "ture").siblings().attr("aria-disabled", "false");
+
+      if($(this).hasClass('pause_btn')){
         mainSwiper.autoplay.stop();
-      })
-      $('.restart_btn').click(function(){
-        $('.pause_btn').show();
-        $('.restart_btn').hide();
-        mainSwiper.autoplay.play();
-      })
+      }else {
+        mainSwiper.autoplay.start();
+      }
+    })
 
-      // header menu swiper
-      var headerMenuSwiper = new Swiper(".header_swiper", {
-          slidesPerView: 'auto',
-          freeMode: true,
-      });
+    // header menu swiper
+    const headerMenuSwiper = new Swiper(".header_swiper", {
+        slidesPerView: 'auto',
+        freeMode: true,
+    });
 
-      // 스크롤시 헤더부분 움직임
-      $(window).scroll(function(){
-        realScrollNum = $(document).scrollTop();
+    // 스크롤시 헤더부분 움직임
+    $(window).scroll(function(){
+      realScrollNum = $(document).scrollTop();
 
-        if(realScrollNum > 0) {
-          $('header').addClass('on');
-        } else {
-          $('header').removeClass('on');
-        }
-      })
+      if(realScrollNum > 0) {
+        $('header').addClass('on');
+      } else {
+        $('header').removeClass('on');
+      }
+    })
 
-      // 헤더 메뉴 클릭시 색상변경
-      $('.menu_item').click(function(){
-        $(this).addClass('active').siblings().removeClass('active');
-      })
+    // 헤더 메뉴 클릭시 색상변경
+    $('.menu_item').click(function(){
+      $(this).addClass('active').siblings().removeClass('active');
+    })
 
-      // main visual swiper
-    var scrollMenuSwiper = new Swiper(".menu_swiper", {
+    // main visual swiper
+    const scrollMenuSwiper = new Swiper(".menu_swiper", {
       slidesPerView: "auto",
       scrollbar: {
         el: ".swiper-scrollbar",
@@ -64,7 +63,7 @@ $(function(){
     });
 
     // header menu swiper
-    var recMenuSwiper = new Swiper(".rec_menu_swiper", {
+    const recMenuSwiper = new Swiper(".rec_menu_swiper", {
       slidesPerView: 'auto',
       freeMode: true,
     });
@@ -87,13 +86,11 @@ $(function(){
     setInterval(todayEventTimeFn,1000);
 
     // aside swiper
-    var asideSwiper = new Swiper(".aside_swiper", {
-      //initialSlide: 0,
+    const asideSwiper = new Swiper(".aside_swiper", {
       spaceBetween: 10,
       slidesPerView: 'auto',
       centeredSlides: true,
       loop: true,
-      loopedSlides: 1,
       loopAdditionalSlides : 1,
       autoplay: {
               delay: 2000,
@@ -106,7 +103,7 @@ $(function(){
     });
 
     // aside pic swiper
-    var asidePicSwiper = new Swiper(".aside_pic_swiper", {
+    const asidePicSwiper = new Swiper(".aside_pic_swiper", {
       spaceBetween: 10,
       slidesPerView: 1.5,
       centeredSlides: true,
@@ -140,7 +137,7 @@ $(function(){
     setInterval(powerDealTimeFn,1000);
 
     // 공식 브랜드관 swiper
-    var brandSwiper = new Swiper(".brand_swiper", {
+    const brandSwiper = new Swiper(".brand_swiper", {
       spaceBetween: 15,
       slidesPerView: 'auto',
       freeMode: true,
@@ -148,14 +145,14 @@ $(function(){
 
 
     // 베스트셀러 swiper
-    var bestSwiper = new Swiper(".bestseller_swiper", {
+    const bestSwiper = new Swiper(".bestseller_swiper", {
       spaceBetween: 10,
       slidesPerView: 'auto',
       freeMode: true,
     });
 
     // 결제 할인 swiper
-    var bestSwiper = new Swiper(".pay_swiper", {
+    const paySwiper = new Swiper(".pay_swiper", {
       spaceBetween: 10,
       slidesPerView: 'auto',
       freeMode: true,
@@ -163,15 +160,14 @@ $(function(){
 
 
     // 카테고리 swiper
-    var categorySwiper = new Swiper(".category_swiper", {
+    const categorySwiper = new Swiper(".category_swiper", {
       slidesPerView: 'auto',
       freeMode: true,
     });
 
     // 스크롤 내리면 픽스버튼
     $(".container").on('mousewheel',function(e){
-      var wheel = e.originalEvent.wheelDelta;
-      //console.log(wheel)
+      let wheel = e.originalEvent.wheelDelta;
       
         if(wheel>0){
         //스크롤 올릴때
